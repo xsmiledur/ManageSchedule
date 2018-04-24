d4c67a66914fb1157e4d28cf409dbb9e28bd9631
Yuri Mikawa <xsmiledur.8x7@gmail.com>
2018-04-25 07:54:00 +0900

    First Commit

---
+++ application/modules/default/controllers/DetailController.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * DetailController
+ */
+require_once 'Zend/Controller/Action.php';
+require_once 'Zend/Config/Ini.php';
+require_once 'Zend/Session.php';
+
+require_once '../application/modules/default/models/MainModel.php';
+
+class DetailController extends Zend_Controller_Action
+{
+    private $_config;                         // 設定情報
+    private $_session;                        // セッションのインスタンス
+    private $_lang;                           // 言語設定
+    private $_contents;                       // 言語データ
+    private $_main;                           // モデルのインスタンス
+    public $view;
+
+    public function init()
+    {
+        /**
+         * ドメイン設定とスペースの確認
+         */
+        // 基本セッションを構築
+
+        $this->_session = new Zend_Session_Namespace('data');
+
+        // 設定情報をロードする
+        $this->_config = new Zend_Config_Ini('../application/modules/default/lib/config.ini', null);
+
+        /**
+         * DBの接続
+         */
+
+        $db_rand = rand(1,2);
+        if ($db_rand == 2) {
+            $db_read = $this->_config->datasourceread2->database->toArray();
+        } else {
+            $db_read = $this->_config->datasourceread1->database->toArray();
+        }
+
+        $db_write = $this->_config->datasource->database->toArray();
+
+        // モデルのインスタンスを生成する
+        $this->_main = new MainModel($db_read,$db_write);
+
+        /**
+         * 言語データを取得する
+         */
+        // 言語指定を確認
+
+        if ($this->getRequest()->getParam('la')) {
+            $this->_session->lang = $this->getRequest()->getParam('la');
+        } elseif(!$this->_session->lang) {
+            $this->_session->lang = 'ja';
+        }
+        $this->_lang = $this->_session->lang;
+
+        /**
+         * Viewに必要データを渡す
+         */
+
+        // メッセージがあればviewに渡す
+        $this->view->successMsg = $this->_session->successMsg;
+        $this->view->errMsg = $this->_session->errMsg;
+        unset($this->_session->successMsg);
+        unset($this->_session->errMsg);
+
+        // pathとユーザー情報をviewに渡す
+        $this->view->path       = $this->getRequest()->getPathInfo();
+        $this->view->lang       = $this->_session->lang;
+
+        //$this->_helper->layout->setLayout('index');
+
+    }
+
+    public function indexAction()
+    {
+
+        $request = $this->getRequest();
+        $id = $request->getParam("id");
+        $flg = true;
+        $memName = NULL;
+        if ($id != NULL) {
+            $memName = $this->_main->getGroupData($id);
+            $flg = ($memName != NULL);
+        }
+
+        if ($flg) {
+            $_num = $request->getPost("num");
+            $num = 0;
+            $memName = array();
+            for ($i = 1; $i <= $_num; $i++) {
+                $test = $request->getPost("mem-name" . (string)$i);
+                if ($test != NULL) {
+                    ++$num;
+                    array_push($memName, $test);
+                }
+            }
+
+        }
+
+        $this->view->memName = $memName;
+
+    }
+
+
+}
